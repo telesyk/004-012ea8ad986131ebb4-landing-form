35b0afe6f823429201e6f344b285db1684a786f5
telesyk <telesyk89@gmail.com>
2023-02-09 14:02:09 +0200

    Update UI with Error and isLoading states; Add fake fetch for data.

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,17 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 import classNames from 'classnames';
 import { isMobile } from 'react-device-detect';
+import useFetch from 'react-fetch-hook';
+import { dataUrl } from './constants';
 import SelectNative from './components/SelectNative';
 import logotype from './assets/logotype.svg';
-
-const OPTIONS = [
-  { value: 'full-stack developer', label: 'Full-stack developer' },
-  { value: 'front-end developer', label: 'Front-end developer' },
-  { value: 'back-end developer', label: 'Back-end developer' },
-  { value: 'design role', label: 'Design role' },
-  { value: 'product role', label: 'Product role' },
-  { value: 'other', label: 'Other' },
-];
-
-const CAPTIONS = {
-  logo: {
-    src: '/src/assets/logotype.svg',
-    text: 'websote logotype',
-  },
-  form: {
-    description:
-      'The human-first, 100% no-code API for fully managed backend services 😻',
-    topText: 'Get on our updates list',
-    successResponse: 'Done. You can unsubscribe any time via the email.',
-    submitText: 'Notify me',
-  },
-  placeholder: {
-    select: 'Role',
-    email: 'Email',
-  },
-  footer: {
-    description1:
-      'We are working to simplify building premium web, mobile and IoT apps by taking the geek speak and maintenance out of the backend services required to deliver smart and engaging applications.',
-    description2:
-      'By the way, this page is powered by our solution. If you are interested in a demo or would like to have a chat drop us a line at demo at includecore.com.',
-    copyrights: '© 2023 IncludeCore. All rights reserved.',
-  },
-};
 
 function App() {
   const [selectedOption, setSelectedOption] = useState(null);
   const [emailValue, setEmailValue] = useState('');
   const [isSuccessResponse, setIsSuccessResponse] = useState(null);
+
+  const { isLoading, data, error } = useFetch(dataUrl);
 
   // eslint-disable-next-line no-unused-vars
   const checkValidation = data => {
@@ -78,99 +48,133 @@ function App() {
     setSelectedOption(option);
   };
 
+  const renderLoading = () => (
+    <div className="app-top">
+      <div className="app-loading">
+        <p>The data is loading...</p>
+      </div>
+    </div>
+  );
+
+  const renderError = err => (
+    <div className="app-top">
+      <div className="app-error">
+        <p className="app-error-title">
+          <strong>There is an error:</strong>
+        </p>
+        <pre className="app-error-message">
+          <code>{err}</code>
+        </pre>
+      </div>
+    </div>
+  );
+
+  const renderContent = allData => {
+    const { options, captions } = allData;
+    return (
+      <>
+        <div className="app-top">
+          <div className="app-container">
+            <div className="app-logotype">
+              <img src={logotype} alt={captions.logo.text} />
+            </div>
+            {!isSuccessResponse ? (
+              <>
+                <p className="app-top-description">
+                  {captions.form.description}
+                </p>
+                <form onSubmit={handleFormSubmit} className="app-form form">
+                  <p className="form-text-top">{captions.form.topText}</p>
+                  <div className="form-main">
+                    <div className="form-controls">
+                      {isMobile ? (
+                        <div className="form-control-panel form-select-native__container">
+                          <SelectNative
+                            options={options}
+                            className="form-select-native"
+                            id="roleList"
+                            onChange={handleSelectChange}
+                          />
+                        </div>
+                      ) : (
+                        <div className="form-control-panel">
+                          <Select
+                            className="form-select"
+                            classNamePrefix="form-select"
+                            placeholder={captions.placeholder.select}
+                            defaultValue={selectedOption}
+                            onChange={handleSelectChange}
+                            options={options}
+                          />
+                        </div>
+                      )}
+                      <div className="form-control-panel">
+                        <input
+                          className="form-control"
+                          type="email"
+                          placeholder={captions.placeholder.email}
+                          required
+                          onChange={handleEmail}
+                          value={emailValue}
+                        />
+                      </div>
+                    </div>
+                    <p className="form-text-bottom">
+                      This form is protected by reCAPTCHA, and the Google{' '}
+                      <a
+                        href="https://policies.google.com/privacy"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Privacy Policy
+                      </a>{' '}
+                      and{' '}
+                      <a
+                        href="https://policies.google.com/terms"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Terms of Service
+                      </a>{' '}
+                      apply.
+                    </p>
+                    <button
+                      className="form-submit"
+                      type="submit"
+                      onClick={handleFormSubmit}
+                    >
+                      {captions.form.submitText}
+                    </button>
+                  </div>
+                </form>
+              </>
+            ) : (
+              <p className={classNames('app-top-description', 'is-success')}>
+                {captions.form.successResponse}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="app-bottom">
+          <div className="app-container">
+            <div className="app-bottom-description">
+              <p>{captions.footer.description1}</p>
+              <p>{captions.footer.description2}</p>
+            </div>
+            <div className="app-bottom-copyrights">
+              <p>{captions.footer.copyrights}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="app">
-      <div className="app-top">
-        <div className="app-container">
-          <div className="app-logotype">
-            <img src={logotype} alt={CAPTIONS.logo.text} />
-          </div>
-          {!isSuccessResponse ? (
-            <>
-              <p className="app-top-description">{CAPTIONS.form.description}</p>
-              <form onSubmit={handleFormSubmit} className="app-form form">
-                <p className="form-text-top">{CAPTIONS.form.topText}</p>
-                <div className="form-main">
-                  <div className="form-controls">
-                    {isMobile ? (
-                      <div className="form-control-panel form-select-native__container">
-                        <SelectNative
-                          options={OPTIONS}
-                          className="form-select-native"
-                          id="roleList"
-                          onChange={handleSelectChange}
-                        />
-                      </div>
-                    ) : (
-                      <div className="form-control-panel">
-                        <Select
-                          className="form-select"
-                          classNamePrefix="form-select"
-                          placeholder={CAPTIONS.placeholder.select}
-                          defaultValue={selectedOption}
-                          onChange={handleSelectChange}
-                          options={OPTIONS}
-                        />
-                      </div>
-                    )}
-                    <div className="form-control-panel">
-                      <input
-                        className="form-control"
-                        type="email"
-                        placeholder={CAPTIONS.placeholder.email}
-                        required
-                        onChange={handleEmail}
-                        value={emailValue}
-                      />
-                    </div>
-                  </div>
-                  <p className="form-text-bottom">
-                    This form is protected by reCAPTCHA, and the Google{' '}
-                    <a
-                      href="https://policies.google.com/privacy"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Privacy Policy
-                    </a>{' '}
-                    and{' '}
-                    <a
-                      href="https://policies.google.com/terms"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Terms of Service
-                    </a>{' '}
-                    apply.
-                  </p>
-                  <button
-                    className="form-submit"
-                    type="submit"
-                    onClick={handleFormSubmit}
-                  >
-                    {CAPTIONS.form.submitText}
-                  </button>
-                </div>
-              </form>
-            </>
-          ) : (
-            <p className={classNames('app-top-description', 'is-success')}>
-              {CAPTIONS.form.successResponse}
-            </p>
-          )}
-        </div>
-      </div>
-      <div className="app-bottom">
-        <div className="app-container">
-          <div className="app-bottom-description">
-            <p>{CAPTIONS.footer.description1}</p>
-            <p>{CAPTIONS.footer.description2}</p>
-          </div>
-          <div className="app-bottom-copyrights">
-            <p>{CAPTIONS.footer.copyrights}</p>
-          </div>
-        </div>
-      </div>
+      {isLoading && renderLoading()}
+      {error && !data && renderError(error)}
+      {!error && !isLoading && renderContent(data)}
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+const dataUrl = './data.json';
+
+export { dataUrl };
